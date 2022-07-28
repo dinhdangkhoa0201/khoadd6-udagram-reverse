4f590f158ebf80653a9d811a0bda6b09d080c996
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-07-28 21:56:23 +0700

    Modify deployDocker

--- deployDocker.sh
+++ deployDocker.sh
@@ -1,4 +1,4 @@
 docker build -t reverseproxy .
-#docker tag udagram-reverseproxy dinhdangkhoa0201/udagram-reverseproxy
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker push dinhdangkhoa0201/udagram-reverseproxy
+docker tag reverseproxy dinhdangkhoa0201/reverseproxy
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push dinhdangkhoa0201/reverseproxy
